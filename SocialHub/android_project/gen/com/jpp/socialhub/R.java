259b52a5f96f72b6caaed30e7f9c92c83c76b268
perettijuan <peretti.juan@gmail.com>
2013-09-24 16:31:29 -0300

    Added navigation drawer

--- SocialHub/android_project/gen/com/jpp/socialhub/R.java
+++ SocialHub/android_project/gen/com/jpp/socialhub/R.java
@@ -971,7 +971,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int action_bar_gradient=0x7f020057;
-        public static final int ic_launcher=0x7f020058;
+        public static final int drawer_shadow=0x7f020058;
+        public static final int ic_drawer=0x7f020059;
+        public static final int ic_launcher=0x7f02005a;
     }
     public static final class id {
         public static final int action_bar=0x7f05001a;
@@ -992,9 +994,11 @@ containing a value of this type.
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002b;
         public static final int collapseActionView=0x7f05000d;
+        public static final int content_frame=0x7f050043;
         public static final int default_activity_button=0x7f050026;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int drawer_layout=0x7f050042;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050036;
         public static final int end=0x7f050013;
@@ -1005,6 +1009,7 @@ containing a value of this type.
         public static final int icon=0x7f050028;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050025;
+        public static final int left_drawer=0x7f050044;
         public static final int left_icon=0x7f050031;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050027;
@@ -1091,7 +1096,8 @@ containing a value of this type.
         public static final int abc_screen=0x7f030015;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030016;
         public static final int abc_search_view=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int main_screen=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1135,6 +1141,8 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
+        public static final int close=0x7f0a000f;
+        public static final int open=0x7f0a000e;
     }
     public static final class style {
         /** 
