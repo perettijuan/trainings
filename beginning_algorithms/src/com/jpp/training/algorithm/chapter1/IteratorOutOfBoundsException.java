ce96bb91177c206d1fb49b132236e90fbe245dd8
perettijuan <peretti.juan@gmail.com>
2014-01-29 11:30:14 -0300

    removed

--- beginning_algorithms/src/com/jpp/training/algorithm/chapter1/IteratorOutOfBoundsException.java
+++
@@ -1,9 +0,0 @@
-package com.jpp.training.algorithm.chapter1;
-
-public class IteratorOutOfBoundsException extends RuntimeException {
-  
-    private static final long serialVersionUID = 8934032319043548649L;
-    
-    
-
-}
