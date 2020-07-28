2752aa7c35b2ac150e9350d2174bf1856d493db9
jppearnin <juan.peretti@earnin.com>
2020-07-28 15:53:45 -0300

    Added JumpinOnClouds

---
+++ cracking_the_code_interview_6/test/hackerrank/warmup/JumpinOnCloudsTest.java
@@ -0,0 +1,50 @@
+package hackerrank.warmup;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JumpinOnCloudsTest {
+
+	/*
+	 * 0 0 1 0 0 1 0
+	 * 4
+	 */
+	@Test
+	public void test1() {	
+		int[] c = new int[] { 0, 0, 1, 0, 0, 1, 0 };
+		
+		int expected = 4;
+		int actual = JumpinOnClouds.jumpingOnClouds(c);
+		
+		assertEquals(expected, actual);
+	}
+	
+	/*
+	 * 0 0 0 1 0 0 1 0
+	 * 4
+	 */
+	@Test
+	public void test2() {	
+		int[] c = new int[] { 0, 0, 0, 1, 0, 0, 1, 0 };
+		
+		int expected = 4;
+		int actual = JumpinOnClouds.jumpingOnClouds(c);
+		
+		assertEquals(expected, actual);
+	}
+	
+	/*
+	 * 0 0 0 0 0 0 0
+	 * 4
+	 */
+	@Test
+	public void test3() {	
+		int[] c = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+		
+		int expected = 3;
+		int actual = JumpinOnClouds.jumpingOnClouds(c);
+		
+		assertEquals(expected, actual);
+	}
+}
