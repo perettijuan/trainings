59445c97e97dd9c86e95dca9a9b18c463c430dee
jppearnin <juan.peretti@earnin.com>
2020-08-01 18:40:49 -0300

    Added implementation for MToLastElement

--- cracking_the_code_interview_6/src/hackerrank/challenges/MToLastElement.java
+++ cracking_the_code_interview_6/src/hackerrank/challenges/MToLastElement.java
@@ -1,27 +1,54 @@
 package hackerrank.challenges;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
+/*
+ * M = 2
+ * List = 5 2 1 8
+ * O = 1
+ * 
+ * M = 4
+ * List = 10 200 3 40000 5
+ * O = 4 
+ * 
+ * M = 2
+ * List = 42
+ * O = Nil
+ *
+ * M = 3
+ * List = 1 7 6
+ * O = 1
+ * 
+ * M = 7
+ * List = 1 7 6 5 8 3 10 87 1000 76
+ * O = 6
+ * 
+ * Item to return = list.length - M // list.lenght starts at 1
+ * 
+ * 1 - Iterate the list and count using a counter (counter starts at 1).
+ * 2 - Have a HashMap key = counter, value = node
+ * 3 - When done, calculate counter - M = value in hashMap
+ * 4 - Get from HasMap
+ *
+ *
+ *
+ * ********************************** TOOK ME 45 MIN APROX **********************************************
+ */
 public class MToLastElement {
 
+	/*
+	 * This is the code to read the input for:
+	 * https://www.hackerrank.com/contests/programming-interview-questions/
+	 * challenges/m-th-to-last-element
+	 */
 	public static void main(String[] args) {
-		/*
-		 * This is the code to read the input for:
-		 * https://www.hackerrank.com/contests/programming-interview-questions/challenges/m-th-to-last-element
-		 * 
-		 */
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String line = br.readLine();
 
 			int m = Integer.parseInt(line);
-			List<Integer> input = new ArrayList();
+			List<Integer> input = new ArrayList<Integer>();
 
 			StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
 			while (tokenizer.hasMoreTokens()) {
@@ -31,11 +58,21 @@ public class MToLastElement {
 			// these two are the inputs
 			System.out.println("m = " + m);
 			System.out.println("input = " + input.toString());
-			
+
 			// this is the output
 			System.out.println("9");
 			
-			int outPut = findMToLastelement(input, m);
+			KNode head = new KNode(input.get(0));
+			KNode current = head;
+			for (int i = 1; i < input.size(); i++) {
+				if (current == null) {
+					break;
+				}
+				current.mNext = new KNode(input.get(i));
+				current = current.mNext;
+			}
+
+			int outPut = findMToLastelement(head, m);
 			System.out.println(outPut);
 
 		} catch (Exception e) {
@@ -43,10 +80,36 @@ public class MToLastElement {
 		}
 
 	}
+
+	public static int findMToLastelement(KNode head, int m) { // 10 200 3 40000 5, M = 4
+		int counter = 0;
+		HashMap<Integer, KNode> map = new HashMap<Integer, MToLastElement.KNode>();
+		
+		KNode current = head; //current = 200
+		while (current != null) { 
+			counter = counter + 1; // counter = 2
+			map.put(counter, current); // map = [1, 10]
+			current = current.mNext; // current = 200
+		}
+		
+		int positionNode = (counter + 1) - m; // 4 - 2 = 2
+		
+		if (positionNode <= 0) {
+			return -1;
+		} else {
+			return map.get(positionNode).mValue;
+		}
+	}
 	
 	
-	public static int findMToLastelement(List<Integer> input, int m) {
-		return 1;
+	
+	public static class KNode {
+		public KNode mNext;
+		public int mValue;
+		
+		public KNode(int value) {
+			mValue = value;
+		}
 	}
 
 }
---
+++ cracking_the_code_interview_6/test/challenges/MToLastElementTest.java
@@ -0,0 +1,176 @@
+package challenges;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import hackerrank.challenges.MToLastElement;
+import hackerrank.challenges.MToLastElement.KNode;
+
+public class MToLastElementTest {
+
+	/*
+	 * M = 2
+	 * List = 5 2 1 8
+	 * O = 1
+	 */
+	@Test
+	public void test1() {
+		List<Integer> input = new ArrayList<Integer>();
+		input.add(5);
+		input.add(2);
+		input.add(1);
+		input.add(8);
+		
+		KNode head = listFrom(input);
+		
+		int expected = 1;
+		int m = 2;
+		
+		int actual = MToLastElement.findMToLastelement(head, m); 
+		
+		assertEquals(expected, actual);
+	}
+	
+	/*
+	 * M = 4
+	 * List = 10 200 3 40000 5
+	 * O = 200
+	 */
+	@Test
+	public void test2() {
+		List<Integer> input = new ArrayList<Integer>();
+		input.add(10);
+		input.add(200);
+		input.add(3);
+		input.add(40000);
+		input.add(5);
+		
+		KNode head = listFrom(input);
+		
+		int expected = 200;
+		int m = 4;
+		
+		int actual = MToLastElement.findMToLastelement(head, m); 
+		
+		assertEquals(expected, actual);
+	}
+	
+	
+	/*
+	 * M = 2
+	 * List = 42
+	 * O = Nil
+	 */
+	@Test
+	public void test3() {
+		List<Integer> input = new ArrayList<Integer>();
+		input.add(42);
+		
+		KNode head = listFrom(input);
+		
+		int expected = -1;
+		int m = 2;
+		
+		int actual = MToLastElement.findMToLastelement(head, m); 
+		
+		assertEquals(expected, actual);
+	}
+	
+	/*
+	 * M = 2
+	 * List = 42
+	 * O = Nil
+	 */
+	@Test
+	public void test4() {
+		List<Integer> input = new ArrayList<Integer>();
+		input.add(1);
+		input.add(7);
+		input.add(6);
+		
+		KNode head = listFrom(input);
+		
+		int expected = 1;
+		int m = 3;
+		
+		int actual = MToLastElement.findMToLastelement(head, m); 
+		
+		assertEquals(expected, actual);
+	}
+	
+	/*
+	 * M = 7
+	 * List = 1 7 6 5 8 3 10 87 1000 76
+	 * O = 6
+	 */
+	@Test
+	public void test5() {
+		List<Integer> input = new ArrayList<Integer>();
+		input.add(1);
+		input.add(7);
+		input.add(6);
+		input.add(5);
+		input.add(8);
+		input.add(3);
+		input.add(10);
+		input.add(87);
+		input.add(1000);
+		input.add(76);
+		
+		KNode head = listFrom(input);
+		
+		int expected = 5;
+		int m = 7;
+		
+		int actual = MToLastElement.findMToLastelement(head, m); 
+		
+		assertEquals(expected, actual);
+	}
+	
+	/*
+	 * M = 1
+	 * List = 1 7 6 5 8 3 10 87 1000 76
+	 * O = 76
+	 */
+	@Test
+	public void test6() {
+		List<Integer> input = new ArrayList<Integer>();
+		input.add(1);
+		input.add(7);
+		input.add(6);
+		input.add(5);
+		input.add(8);
+		input.add(3);
+		input.add(10);
+		input.add(87);
+		input.add(1000);
+		input.add(76);
+		
+		KNode head = listFrom(input);
+		
+		int expected = 76;
+		int m = 1;
+		
+		int actual = MToLastElement.findMToLastelement(head, m); 
+		
+		assertEquals(expected, actual);
+	}
+	
+	
+	private KNode listFrom(List<Integer> input) {
+		KNode head = new KNode(input.get(0));
+		KNode current = head;
+		for (int i = 1; i < input.size(); i++) {
+			if (current == null) {
+				break;
+			}
+			current.mNext = new KNode(input.get(i));
+			current = current.mNext;
+		}
+		return head;
+	}
+}
