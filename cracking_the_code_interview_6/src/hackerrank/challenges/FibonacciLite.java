8a6770772542d799eabdabe2b05c4846271d95df
jppearnin <juan.peretti@earnin.com>
2020-08-03 15:12:50 -0300

    Added FibonacciLite implementation

--- cracking_the_code_interview_6/src/hackerrank/challenges/FibonacciLite.java
+++ cracking_the_code_interview_6/src/hackerrank/challenges/FibonacciLite.java
@@ -5,7 +5,14 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-
+/*
+ *
+ *  Input = 12, Output = 144
+ *   Fibonacci(12) => 0, 1, 1, 2, 3, 5, 8, 13, 21 34 55  89  144
+ *                    0  1  2  3  4  5  6   7  8  9  10  11  12 
+ *  
+ *  Edge case = Input = 1, Output = 1
+ */
 public class FibonacciLite {
 
 	/*
@@ -18,13 +25,33 @@ public class FibonacciLite {
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String line = br.readLine();
-			int n = Integer.parseInt(line);
+			int target = Integer.parseInt(line);
 			// input
-			System.out.println("n = " + n);
+			//System.out.println("n = " + n);
 			// output
-			System.out.println(1);
+			System.out.println(fibonacciFor(target));
 		} catch (Exception e) {
 
 		}
+	}
+	
+	
+	// base case
+	public static int fibonacciFor(int target) { // target = 12 
+		return fibonacci(0, 1, 0, target); 
+	}
+	
+	
+	private static int fibonacci(int n0, int n1, int progression, int target) { // n0 = 55, n1 = 89, progression = 10, target = 12
+		if (progression == target) { // true
+			return n0;
+		}
+		
+		int next = n0 + n1; // next = 144
+		int nextProgression = progression + 1; // 11
+		
+		
+		
+		return fibonacci(n1, next, nextProgression, target); // 55, 89, 10, 12
 	}
 }
---
+++ cracking_the_code_interview_6/test/challenges/FibonacciLiteTest.java
@@ -0,0 +1,44 @@
+package challenges;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import hackerrank.challenges.FibonacciLite;
+
+public class FibonacciLiteTest {
+	
+
+	@Test
+	public void test1() {
+		int expected = 144;
+		int actual = FibonacciLite.fibonacciFor(12);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test2() {
+		int expected = 832040;
+		int actual = FibonacciLite.fibonacciFor(30);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test3() {
+		int expected = 1;
+		int actual = FibonacciLite.fibonacciFor(1);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test4() {
+		int expected = 0;
+		int actual = FibonacciLite.fibonacciFor(0);
+		
+		assertEquals(expected, actual);
+	}
+
+}
