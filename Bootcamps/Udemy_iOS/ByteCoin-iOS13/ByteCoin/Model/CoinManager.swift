6353f7eac72f7b103dc8eaa357166a8205a9231f
Juan Peretti <juan.peretti@juan.perettiMacBookPro18,1>
2025-01-16 17:09:27 +0000

    Check in ByteCoin

---
+++ Bootcamps/Udemy_iOS/ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,22 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "3c2fb8b2-9901-4f53-a435-cda2afe7075f"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+    func getCoinPrice(for currency: String) {
+        let urlString = ""
+    }
+}
