e25be78452f1f90dbb1934e84308f0bacfff5c11
Juan Peretti <juan.peretti@juan.perettiMacBookPro18,1>
2025-01-22 16:26:31 +0000

    My First SWiftUI

---
+++ Bootcamps/Udemy_iOS/iAmRichSwiftUi/I Am Rich/I Am Rich/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Juan Peretti on 22/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Bootcamps/Udemy_iOS/iAmRichSwiftUi/I Am Rich/I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Juan Peretti on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
