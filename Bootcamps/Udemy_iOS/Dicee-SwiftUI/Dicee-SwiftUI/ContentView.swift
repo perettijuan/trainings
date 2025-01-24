2ed30ad3eec8bc4e02c830a4122390d992808c5d
Juan Peretti <juan.peretti@juan.perettiMacBookPro18,1>
2025-01-24 16:34:00 +0000

    Added Dicee-SwiftUI

---
+++ Bootcamps/Udemy_iOS/Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Juan Peretti on 24/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }.padding(.vertical)
+        }
+    }
+}
+
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Bootcamps/Udemy_iOS/Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Juan Peretti on 24/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
