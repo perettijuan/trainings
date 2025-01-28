aca13793bdac24eee9ce58c47cf3c674f52edcd6
Juan Peretti <juan.peretti@juan.perettiMacBookPro18,1>
2025-01-28 11:24:48 +0000

    Remove unused feature flag

---
+++ Bootcamps/Udemy_iOS/Todoey/Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by Juan Peretti on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
