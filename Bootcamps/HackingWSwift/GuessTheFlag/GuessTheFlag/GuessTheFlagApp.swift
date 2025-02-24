ae6f8c4ee6b476de6d6e11d1bae998ccf58a189d
Juan Peretti <juan.peretti@juan.perettiMacBookPro18,1>
2025-02-24 15:03:32 +0000

    Added GuestTheFlag

---
+++ Bootcamps/HackingWSwift/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Juan Peretti on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
